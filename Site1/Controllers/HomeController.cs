0981aa14f2d70e619223e011c1a3bd539e31fe7c
andriy1234 <andriy1234@A34COMP7>
2019-02-13 17:43:25 +0200

    Add project files.

---
+++ Site1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAL;
+namespace Site1.Controllers
+{
+    public class HomeController : Controller
+    {
+        DAL.DBase ctx = new DAL.DBase();
+        public ActionResult Index()
+        {
+            return View(ctx.Tasks);
+        }
+        public ActionResult Create()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            foreach (var el in ctx.Priorities)
+                items.Add(new SelectListItem { Value = el.ID.ToString(), Text = el.Name });
+            PriorityAndTaskModel priorityAndTasksModel = new PriorityAndTaskModel()
+            {
+                SelectListItems = items,
+                task = new Tasks()
+            };
+            return View(priorityAndTasksModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+      
+
+    }
+}
---
+++ Site1/Models/PriorityAndTaskModel.cs
@@ -0,0 +1,19 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site1
+{
+    public class PriorityAndTaskModel
+    {
+        public PriorityAndTaskModel()
+        {
+            SelectListItems = new List<SelectListItem>();
+        }
+        public IEnumerable<SelectListItem> SelectListItems { get; set; }
+        public Tasks task { get; set; }
+    }
+}
